45631e2cd00c19e92e24a471a976409ce8fd5f53
bramo <bmoosterlee@gmail.com>
2018-03-14 15:15:58 +0100

    Fix computedQuestion to correctly represent evaluator results

--- src/main/java/ql/gui/ComputedQuestionElement.java
+++ src/main/java/ql/gui/ComputedQuestionElement.java
@@ -1,6 +1,7 @@
 package ql.gui;
 
 import ql.ast.statements.ComputedQuestion;
+import ql.evaluator.Evaluatable;
 import ql.evaluator.Evaluator;
 
 import java.awt.*;
@@ -18,8 +19,15 @@ public class ComputedQuestionElement extends GUIElement {
 
     @Override
     public void render(Graphics g) {
+        System.out.println("Rendering computedQuestion");
         String varName = node.getId();
-        Object value = evaluator.get(node);
+        Object value = "";
+
+        Evaluatable evaluatable = evaluator.get(node);
+        if(evaluatable!=null) {
+            value = evaluatable.getValue();
+        }
+
         g.setColor(Color.black);
         g.drawString(varName + ": " + String.valueOf(value), 50, yLocation);
     }
