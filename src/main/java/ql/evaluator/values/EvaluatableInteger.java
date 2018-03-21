4f2020b8cb6fbf2498ef20d0fd28d8c8ae28b946
bramo <bmoosterlee@gmail.com>
2018-03-21 17:00:41 +0100

    Move double dispatch methods to subclasses to prevent infinte loop

--- src/main/java/ql/evaluator/values/EvaluatableInteger.java
+++ src/main/java/ql/evaluator/values/EvaluatableInteger.java
@@ -69,4 +69,60 @@ public class EvaluatableInteger extends EvaluatableNumeric<Integer> {
     public EvaluatableNumeric arithmeticNegate() {
         return new EvaluatableInteger(-getValue());
     }
+
+    @Override
+    public Evaluatable add(Evaluatable evaluatable) {
+        return evaluatable.add(this);
+    }
+
+    @Override
+    public EvaluatableBoolean and(Evaluatable evaluatable) {
+        return evaluatable.and(this);
+    }
+
+    @Override
+    public Evaluatable divide(Evaluatable evaluatable) {
+        return evaluatable.divide(this);
+    }
+
+    @Override
+    public EvaluatableBoolean greaterThanEqual(Evaluatable evaluatable) {
+        return evaluatable.greaterThanEqual(this);
+    }
+
+    @Override
+    public EvaluatableBoolean greaterThan(Evaluatable evaluatable) {
+        return evaluatable.greaterThan(this);
+    }
+
+    @Override
+    public EvaluatableBoolean equal(Evaluatable evaluatable) {
+        return evaluatable.equal(this);
+    }
+
+    @Override
+    public EvaluatableBoolean lessThanEqual(Evaluatable evaluatable) {
+        return evaluatable.lessThanEqual(this);
+    }
+
+    @Override
+    public EvaluatableBoolean lessThan(Evaluatable evaluatable) {
+        return evaluatable.lessThan(this);
+    }
+
+    @Override
+    public EvaluatableBoolean notEqual(Evaluatable evaluatable) {
+        return evaluatable.notEqual(this);
+    }
+
+    @Override
+    public Evaluatable multiply(Evaluatable evaluatable) {
+        return evaluatable.multiply(this);
+    }
+
+    @Override
+    public Evaluatable subtract(Evaluatable evaluatable) {
+        return evaluatable.subtract(this);
+    }
+
 }
--- src/main/java/ql/evaluator/values/EvaluatableMoney.java
+++ src/main/java/ql/evaluator/values/EvaluatableMoney.java
@@ -93,4 +93,60 @@ public class EvaluatableMoney extends EvaluatableNumeric<BigDecimal> {
         BigDecimal result = getValue().negate();
         return new EvaluatableMoney(result);
     }
+
+    @Override
+    public Evaluatable add(Evaluatable evaluatable) {
+        return evaluatable.add(this);
+    }
+
+    @Override
+    public EvaluatableBoolean and(Evaluatable evaluatable) {
+        return evaluatable.and(this);
+    }
+
+    @Override
+    public Evaluatable divide(Evaluatable evaluatable) {
+        return evaluatable.divide(this);
+    }
+
+    @Override
+    public EvaluatableBoolean greaterThanEqual(Evaluatable evaluatable) {
+        return evaluatable.greaterThanEqual(this);
+    }
+
+    @Override
+    public EvaluatableBoolean greaterThan(Evaluatable evaluatable) {
+        return evaluatable.greaterThan(this);
+    }
+
+    @Override
+    public EvaluatableBoolean equal(Evaluatable evaluatable) {
+        return evaluatable.equal(this);
+    }
+
+    @Override
+    public EvaluatableBoolean lessThanEqual(Evaluatable evaluatable) {
+        return evaluatable.lessThanEqual(this);
+    }
+
+    @Override
+    public EvaluatableBoolean lessThan(Evaluatable evaluatable) {
+        return evaluatable.lessThan(this);
+    }
+
+    @Override
+    public EvaluatableBoolean notEqual(Evaluatable evaluatable) {
+        return evaluatable.notEqual(this);
+    }
+
+    @Override
+    public Evaluatable multiply(Evaluatable evaluatable) {
+        return evaluatable.multiply(this);
+    }
+
+    @Override
+    public Evaluatable subtract(Evaluatable evaluatable) {
+        return evaluatable.subtract(this);
+    }
+
 }
