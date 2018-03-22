15b296d18715420b3c0ec22a433ca9a71621c25c
bramo <bmoosterlee@gmail.com>
2018-03-22 16:47:22 +0100

    Remove redundant methods and added Date type implementations

--- src/main/java/ql/evaluator/values/EvaluatableDate.java
+++ src/main/java/ql/evaluator/values/EvaluatableDate.java
@@ -27,181 +27,52 @@ public class EvaluatableDate implements Evaluatable<Date> {
 
     @Override
     public EvaluatableBoolean greaterThanEqual(Evaluatable evaluatable) {
-        return null;
+        return evaluatable.greaterThanEqual(this);
     }
 
     @Override
     public EvaluatableBoolean greaterThanEqual(EvaluatableDate evaluatable) {
-        return null;
+        return new EvaluatableBoolean(evaluatable.getValue().after(getValue()) || evaluatable.getValue().equals(getValue()));
     }
 
     @Override
     public EvaluatableBoolean greaterThan(Evaluatable evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean greaterThan(EvaluatableMoney evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean greaterThan(EvaluatableDecimal evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean greaterThan(EvaluatableInteger evaluatable) {
-        return null;
+        return evaluatable.greaterThan(this);
     }
 
     @Override
     public EvaluatableBoolean greaterThan(EvaluatableDate evaluatable) {
-        return null;
+        return new EvaluatableBoolean(evaluatable.getValue().after(getValue()));
     }
 
     @Override
     public EvaluatableBoolean lessThanEqual(Evaluatable evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean lessThanEqual(EvaluatableMoney evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean lessThanEqual(EvaluatableDecimal evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean lessThanEqual(EvaluatableInteger evaluatable) {
-        return null;
+        return evaluatable.lessThanEqual(this);
     }
 
     @Override
     public EvaluatableBoolean lessThanEqual(EvaluatableDate evaluatable) {
-        return null;
+        return new EvaluatableBoolean(evaluatable.getValue().before(getValue()) || evaluatable.getValue().equals(getValue()));
     }
 
     @Override
     public EvaluatableBoolean lessThan(Evaluatable evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean lessThan(EvaluatableMoney evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean lessThan(EvaluatableDecimal evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean lessThan(EvaluatableInteger evaluatable) {
-        return null;
+        return evaluatable.lessThan(this);
     }
 
     @Override
     public EvaluatableBoolean lessThan(EvaluatableDate evaluatable) {
-        return null;
-    }
-
-    @Override
-    public Evaluatable multiply(Evaluatable evaluatable) {
-        return null;
-    }
-
-    @Override
-    public Evaluatable multiply(EvaluatableMoney evaluatable) {
-        return null;
-    }
-
-    @Override
-    public Evaluatable multiply(EvaluatableDecimal evaluatable) {
-        return null;
-    }
-
-    @Override
-    public Evaluatable multiply(EvaluatableInteger evaluatable) {
-        return null;
+        return new EvaluatableBoolean(evaluatable.getValue().before(getValue()));
     }
 
     @Override
     public EvaluatableBoolean notEqual(Evaluatable evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean notEqual(EvaluatableMoney evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean notEqual(EvaluatableDecimal evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean notEqual(EvaluatableInteger evaluatable) {
-        return null;
+        return evaluatable.notEqual(this);
     }
 
     @Override
     public EvaluatableBoolean notEqual(EvaluatableDate evaluatable) {
-        return null;
+        return new EvaluatableBoolean(!evaluatable.getValue().equals(getValue()));
     }
 
-    @Override
-    public EvaluatableBoolean notEqual(EvaluatableString evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean notEqual(EvaluatableBoolean evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean or(Evaluatable evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean or(EvaluatableBoolean evaluatable) {
-        return null;
-    }
-
-    @Override
-    public Evaluatable subtract(Evaluatable evaluatable) {
-        return null;
-    }
-
-    @Override
-    public Evaluatable subtract(EvaluatableMoney evaluatable) {
-        return null;
-    }
-
-    @Override
-    public Evaluatable subtract(EvaluatableDecimal evaluatable) {
-        return null;
-    }
-
-    @Override
-    public Evaluatable subtract(EvaluatableInteger evaluatable) {
-        return null;
-    }
-
-    @Override
-    public EvaluatableBoolean logicalNegate() {
-        return null;
-    }
-
-    @Override
-    public EvaluatableNumeric arithmeticNegate() {
-        return null;
-    }
 }
