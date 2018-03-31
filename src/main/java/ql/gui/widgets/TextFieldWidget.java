3773e6592c0447ffa17ff24fbabd3623cbe91cb8
Tim Nederveen <tim.nederveen@hotmail.com>
2018-03-31 10:01:31 +0200

    Implement visitor for JTextfield creation

--- src/main/java/ql/gui/widgets/TextFieldWidget.java
+++ src/main/java/ql/gui/widgets/TextFieldWidget.java
@@ -1,13 +1,22 @@
 package ql.gui.widgets;
 
 import ql.ast.statements.Question;
+import ql.ast.types.*;
+import ql.ast.visitors.TypeVisitor;
 import ql.environment.Environment;
-import ql.environment.values.StringValue;
-import ql.environment.values.Value;
+import ql.environment.values.*;
 import ql.gui.WidgetListener;
 
 import javax.swing.*;
+import javax.swing.text.DateFormatter;
+import javax.swing.text.NumberFormatter;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TextFieldWidget extends BaseWidget {
 
@@ -15,7 +24,7 @@ public class TextFieldWidget extends BaseWidget {
 
     public TextFieldWidget(Environment environment, Question question, boolean isEditable) {
         super(environment, question, isEditable);
-        textField = new JFormattedTextField();
+        textField = createTextField(question);
         textField.setPreferredSize(new Dimension(200, 50));
         setValue();
     }
@@ -35,11 +44,90 @@ public class TextFieldWidget extends BaseWidget {
 
     @Override
     public void registerChangeListener(WidgetListener widgetListener) {
-        textField.addActionListener(e -> widgetListener.onQuestionUpdated(question, new StringValue(textField.getText())));
+        textField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                Value value = null;
+                if (question.isOfType("integer")) {
+                    value = new IntegerValue(Integer.parseInt(textField.getText()));
+                } else if (question.isOfType("decimal")) {
+                    value = new DecimalValue(Double.parseDouble(textField.getText()));
+                } else if (question.isOfType("money")) {
+                    value = new MoneyValue(Double.parseDouble(textField.getText()));
+                } else if (question.isOfType("string")) {
+                    value = new StringValue(textField.getText());
+                }
+                Value finalValue = value;
+                widgetListener.onQuestionUpdated(question, finalValue);
+            }
+        });
+        // textField.addActionListener(event -> widgetListener.onQuestionUpdated(question, finalValue));
     }
 
     @Override
     public JComponent getComponent() {
         return textField;
     }
+
+
+
+    private JFormattedTextField createTextField(Question question) {
+        return question.getType().accept(new TypeVisitor<JFormattedTextField>() {
+            @Override
+            public JFormattedTextField visit(BooleanType booleanType) {
+                return null;
+            }
+
+            @Override
+            public JFormattedTextField visit(DecimalType decimalType) {
+                NumberFormat format = NumberFormat.getInstance();
+                NumberFormatter formatter = new NumberFormatter(format);
+                formatter.setValueClass(Double.class);
+                formatter.setMaximum(Double.MAX_VALUE);
+                formatter.setAllowsInvalid(true);
+                return new JFormattedTextField(formatter);
+            }
+
+            @Override
+            public JFormattedTextField visit(IntegerType integerType) {
+                NumberFormat format = NumberFormat.getInstance();
+                NumberFormatter formatter = new NumberFormatter(format);
+                formatter.setValueClass(Integer.class);
+                formatter.setMaximum(Integer.MAX_VALUE);
+                formatter.setAllowsInvalid(true);
+                format.setGroupingUsed(false);
+                return new JFormattedTextField(formatter);
+            }
+
+            @Override
+            public JFormattedTextField visit(MoneyType moneyType) {
+                NumberFormat format = NumberFormat.getInstance();
+                NumberFormatter formatter = new NumberFormatter(format);
+                formatter.setValueClass(Double.class);
+                formatter.setMaximum(Double.MAX_VALUE);
+                formatter.setAllowsInvalid(true);
+                return new JFormattedTextField(formatter);
+            }
+
+            @Override
+            public JFormattedTextField visit(StringType stringType) {
+                return new JFormattedTextField();
+            }
+
+            @Override
+            public JFormattedTextField visit(DateType dateType) {
+                DateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+                DateFormatter formatter = new DateFormatter(format);
+                formatter.setValueClass(Date.class);
+                formatter.setAllowsInvalid(true);
+                return new JFormattedTextField(formatter);
+            }
+
+            @Override
+            public JFormattedTextField visit(ErrorType errorType) {
+                return null;
+            }
+        });
+    }
+
 }
