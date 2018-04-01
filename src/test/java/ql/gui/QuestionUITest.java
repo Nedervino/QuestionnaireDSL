c82f9dcb64d456f1461730564ff9a5a2b5088aff
Tim Nederveen <tim.nederveen@hotmail.com>
2018-04-02 00:32:34 +0200

    Add not-implemented ui tests

---
+++ src/test/java/ql/gui/QuestionUITest.java
@@ -0,0 +1,24 @@
+package ql.gui;
+
+import org.junit.Test;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import static org.junit.Assert.*;
+
+public class QuestionUITest {
+
+
+    @Test
+    public void canDisplayAllNonComputedQuestionTypes() {
+        //TODO
+        String qlFileName = "src/input/ql/correct/gui/allQuestionTypes.ql";
+        throw new NotImplementedException();
+    }
+
+    @Test
+    public void canDisplayAllComputedQuestionTypes() {
+        String qlFileName = "src/input/ql/correct/gui/allComputedQuestionTypes.ql";
+        throw new NotImplementedException();
+    }
+
+}
