271741d0bdf684f9293af5f95c435603411aeea7
Tim Nederveen <tim.nederveen@hotmail.com>
2018-04-01 12:36:13 +0200

    Remove qls reference from ql main, add requirements example as test form

--- src/main/java/ql/main/Main.java
+++ src/main/java/ql/main/Main.java
@@ -5,8 +5,6 @@ import ql.gui.FormUI;
 import ql.gui.FormUIFactory;
 import ql.parser.FormBuilder;
 import ql.validator.Validator;
-import qls.ast.Stylesheet;
-import qls.parser.StylesheetBuilder;
 
 /**
  * This program parses an input file following QL DSL specification, for which it renders a graphical form
@@ -27,13 +25,6 @@ public class Main {
         FormBuilder formBuilder = new FormBuilder();
         Form form = formBuilder.createForm(qlFile);
 
-        String qlsFileName = "src/input/ql/correct/if.ql";
-        String qlsFile = new FileScanner().loadFile(qlsFileName);
-
-        StylesheetBuilder stylesheetBuilder = new StylesheetBuilder();
-        // Stylesheet stylesheet = stylesheetBuilder.createStylesheet(qlFile);
-        Stylesheet stylesheet = null;
-
         if (Validator.passesChecks(form)) {
             System.out.println("Successfully passed all checks");
             FormUI formUI = new FormUIFactory().getFormUI(form);
@@ -42,7 +33,6 @@ public class Main {
             System.err.println("Form not passing validation");
             System.exit(1);
         }
-
 
     }
 
