3fbc915455168cc37555d7b902b15fa9eefcaee3
bramo <bmoosterlee@gmail.com>
2018-04-01 22:10:37 +0200

    Add question constructor

--- src/main/java/qls/ast/components/Question.java
+++ src/main/java/qls/ast/components/Question.java
@@ -1,11 +1,17 @@
 package qls.ast.components;
 
 import ql.ast.SourceLocation;
+import ql.gui.widgets.Widget;
 
 public class Question extends Component {
 
-    public Question(SourceLocation sourceLocation) {
+    String questionId;
+    Widget widget;
+
+    public Question(String questionId, Widget widget, SourceLocation sourceLocation) {
         super(sourceLocation);
+        this.questionId = questionId;
+        this.widget = widget;
     }
 
 }
