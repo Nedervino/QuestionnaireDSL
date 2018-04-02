d5c2224711759ebba16aaad840d83d34917329f0
bramo <bmoosterlee@gmail.com>
2018-04-02 04:02:43 +0200

    Add tests for creating radiobuttons and spinboxes

--- src/test/java/ql/gui/WidgetFactoryTest.java
+++ src/test/java/ql/gui/WidgetFactoryTest.java
@@ -1,12 +1,57 @@
 package ql.gui;
 
+import issuetracker.IssueTracker;
+import org.junit.Before;
 import org.junit.Test;
+import ql.BaseQlTest;
+import ql.ast.Form;
+import ql.ast.statements.Question;
+import ql.ast.types.BooleanType;
+import ql.ast.types.MoneyType;
+import ql.environment.Environment;
+import ql.environment.FormEnvironment;
+import ql.environment.values.BooleanValue;
+import ql.gui.widgets.RadioWidget;
+import ql.gui.widgets.SpinboxWidget;
+import ql.gui.widgets.Widget;
+import qls.ast.Stylesheet;
+import qls.parser.StylesheetBuilder;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class WidgetFactoryTest {
+import static org.junit.Assert.assertEquals;
+
+public class WidgetFactoryTest extends BaseQlTest {
+
+    private Environment environment;
+
+    @Before
+    public void setUp() throws Exception {
+        issueTracker = new IssueTracker();
+
+        Form form = createForm("src/input/ql/correct/if.ql");
+        environment = new FormEnvironment(form);
+
+        StylesheetBuilder stylesheetBuilder = new StylesheetBuilder();
+        Stylesheet stylesheet = stylesheetBuilder.createStylesheet("src/input/qls/correct/form1.qls");
+
+    }
+
     @Test
-    public void createWidget() throws Exception {
-        throw new NotImplementedException();
+    public void createRadioWidget() {
+        WidgetFactory widgetFactory = new WidgetFactory();
+        Question question = new Question("hasBoughtHouse", "", new BooleanType(null), null);
+        Widget widget = widgetFactory.createWidget(question, environment);
+
+        assertEquals(widget.getClass(), RadioWidget.class);
+    }
+
+    @Test
+    public void createSpinboxWidget() {
+        WidgetFactory widgetFactory = new WidgetFactory();
+        Question question = new Question("sellingPrice", "", new MoneyType(null), null);
+        Widget widget = widgetFactory.createWidget(question, environment);
+
+        assertEquals(widget.getClass(), SpinboxWidget.class);
     }
 
 }
