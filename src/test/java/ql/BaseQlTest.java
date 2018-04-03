0a2a0bdd3868d842d61cfd4d33c6e157fa869747
bramo <bmoosterlee@gmail.com>
2018-04-03 19:10:09 +0200

    Change BaseQlTest to reflect new form creation

--- src/test/java/ql/BaseQlTest.java
+++ src/test/java/ql/BaseQlTest.java
@@ -7,8 +7,7 @@ import ql.utilities.IOHandler;
 public class BaseQlTest {
 
     protected static Form createForm(String fileName) {
-        String fileContent = IOHandler.loadFile(fileName);
-        return FormBuilder.createForm(fileContent);
+        return FormBuilder.createForm(fileName);
     }
 
 }
